6e97b2f86a1b934609527c24ef7e18a3386fad03
Your Name <mayra.lopes@synvia.com>
2021-09-29 18:59:35 -0300

    include frontend

---
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,39 @@
+import { Router, ActivatedRoute } from "@angular/router";
+import { ProductService } from "./../product.service";
+import { Product } from "./../product.model";
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-product-delete",
+  templateUrl: "./product-delete.component.html",
+  styleUrls: ["./product-delete.component.css"],
+})
+export class ProductDeleteComponent implements OnInit {
+
+  product!: Product;
+   
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    public route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    const id = +this.route.snapshot.paramMap.getAll('id');
+    this.productService.readById(id).subscribe((product) => {
+      this.product = product;
+    
+    });
+  }
+
+  deleteProduct() {
+    // this.productService.delete(this.product.id).subscribe(() => {
+    //   this.productService.showMessage("Produto excluido com sucesso!");
+    //   this.router.navigate(["/products"]);
+    // });
+  }
+
+  cancel() {
+    this.router.navigate(["/products"]);
+  }
+}
